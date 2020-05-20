b012f2c32abc67185f27a7d9ed0352a85de9345d
Klopez-5 <kalopez5@espe.edu.ec>
2020-05-20 11:24:41 -0500

    Codigo Inicial

---
+++ src/main/java/com/lopez/ejercicio/PryEjercicioApplication.java
@@ -0,0 +1,15 @@
+package com.lopez.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//se denomina anotacion y ayuda a diferenciar para saber 
+//como se va a comportar dentro de la arquitectura
+@SpringBootApplication
+public class PryEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryEjercicioApplication.class, args);
+	}
+
+}
